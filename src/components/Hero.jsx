ee5b9080665880e0dc8583f03e2bacda3545045e
Samuel <sammylemon69@gmail.com>
2025-07-06 02:30:02 +0100

    minor changes to names

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
   
 import React from 'react'
 import '../styles/Hero.css'
-import heroImg from '../assets/Images/profile.jpg'
+import heroImg from '../assets/images/profile.jpg' /*changed */
 
 const Hero = () => {
   return (
