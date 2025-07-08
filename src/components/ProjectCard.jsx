207b3c143d9e81ff049f9e83de6adba94db03e9a
Samuel <sammylemon69@gmail.com>
2025-07-08 01:46:25 +0100

    modified the links

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,31 @@
-  
-import React from 'react'
-import '../styles/ProjectCard.css'
+import React from 'react';
+import '../styles/ProjectCard.css';
 
 const ProjectCard = ({ project }) => {
+  const handleClick = (e) => {
+    if (project.link === '#') {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="project-card" data-aos="flip-left">
       <img src={project.image} alt={project.title} className="project-img" />
       <div className="project-overlay">
         <h3>{project.title}</h3>
         <p>{project.description}</p>
-        <a href={project.link} target="_blank" rel="noopener noreferrer">
+        <a
+          href={project.link}
+          target={project.link === '#' ? '_self' : '_blank'}
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
           View Project
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
